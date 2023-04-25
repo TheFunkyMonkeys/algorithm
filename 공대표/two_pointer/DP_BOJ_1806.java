c0ffdcd0bad3137298ef794f2f97181255c07e6e
공대표 <c5ngdp@gmail.com>
2023-04-25 21:35:46 +0900

    Algorithm

--- 공대표/two_pointer/DP_BOJ_1806.java
+++ 공대표/two_pointer/DP_BOJ_1806.java
@@ -1,5 +1,7 @@
 package two_pointer;
 
+/* https://www.acmicpc.net/problem/1806 */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,21 +22,28 @@ public class DP_BOJ_1806 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-
-            for (int j = i; j < j + length; j++) {
-                sum += arr[j];
-
-                if (length != 0 && (j - i + 1) > length) {
-                    break;
+        int start = 0, end = 1, sum = arr[start], count = 1;
+        while (start < end) {
+            if (sum >= m) {
+                if (length > count || length == 0) {
+                    length = count;
                 }
-
-                if (sum >= m) {
-                    length = j - i + 1;
-                    break;
-                }
+                sum -= arr[start];
+                start++;
+                count--;
+                continue;
             }
+
+            if (end < n) {
+                sum += arr[end];
+                count++;
+                end++;
+            } else {
+                sum -= arr[start];
+                count--;
+                start++;
+            }
+
         }
 
         System.out.print(length);
---
+++ 공대표/two_pointer/DP_BOJ_1940.java
@@ -0,0 +1,47 @@
+package two_pointer;
+
+/* https://www.acmicpc.net/problem/1940 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class DP_BOJ_1940 {
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int count = 0;
+    
+            int n = Integer.parseInt(br.readLine());
+            int m = Integer.parseInt(br.readLine());
+    
+            int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+    
+            Arrays.sort(arr);
+            int start = 0, end = 1;
+    
+            while (start < n) {
+                if (end >=n) {
+                    start++;
+                    end = start + 1;
+                    continue;
+                }
+    
+                if (arr[start] + arr[end] == m) {
+                    start++;
+                    end = start + 1;
+                    count++;
+                    continue;
+                }
+    
+                end++;
+            }
+    
+            System.out.print(count);
+    }
+}
