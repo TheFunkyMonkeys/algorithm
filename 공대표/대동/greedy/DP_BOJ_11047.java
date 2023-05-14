e65588fdca6eb2fd5c627034910e7718b0e1b4c0
공대표 <c5ngdp@gmail.com>
2023-05-14 20:56:21 +0900

    Algorithm

---
+++ 공대표/대동/greedy/DP_BOJ_11047.java
@@ -0,0 +1,37 @@
+package 대동.greedy;
+
+/* https://www.acmicpc.net/problem/11047 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DP_BOJ_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int index = arr.length - 1;
+        while (k != 0) {
+            if (arr[index] > k) {
+                index -= 1;
+                continue;
+            }
+
+            result += k / arr[index];
+            k %= arr[index];
+        }
+
+        System.out.print(result);
+    }
+}
---
+++ 공대표/대동/greedy/DP_BOJ_1789.java
@@ -0,0 +1,24 @@
+package 대동.greedy;
+
+/* https://www.acmicpc.net/problem/1789 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP_BOJ_1789 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        long sum = 0;
+        long result = 0;
+        while (sum + result <= n) {
+            sum += result;
+            result++;
+        }
+
+        System.out.print(result - 1);
+    }
+}
