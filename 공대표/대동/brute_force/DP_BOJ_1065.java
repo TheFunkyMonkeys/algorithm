cf24e1d83e1ceb5db00cd4cd7844844e32fed9f5
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-05-16 20:02:49 +0900

    Algorithm

---
+++ 공대표/대동/brute_force/DP_BOJ_1065.java
@@ -0,0 +1,29 @@
+package 대동.brute_force;
+
+/* https://www.acmicpc.net/problem/1065 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP_BOJ_1065 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+
+        int x = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= x; i++) {
+            if (i < 100) {
+                result++;
+                continue;
+            }
+
+            int curNum = i;
+            if (curNum % 10 + curNum / 100 == (curNum / 10) % 10 * 2) {
+                result++;
+            }
+        }
+
+        System.out.print(result);
+    }
+}
