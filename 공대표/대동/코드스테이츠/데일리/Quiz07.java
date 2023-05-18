73606d68470d33e3aa8000391efefcfd9914baf0
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-05-18 21:23:19 +0900

    Algorithm

---
+++ 공대표/대동/코드스테이츠/데일리/Quiz07.java
@@ -0,0 +1,38 @@
+package 대동.코드스테이츠.데일리;
+
+import java.util.HashMap;
+
+public class Quiz07 {
+    public static void main(String[] args) {
+        String[][] arr = new String[][]{
+                {"make", "Ford"},
+                {"model", "Mustang"},
+                {"year", "1964"},
+                {"make", "Bill"}
+        };
+
+        HashMap<String, String> output = convertListToHashMap(arr);
+
+
+    }
+
+    public static HashMap<String, String> convertListToHashMap(String[][] arr) {
+        // TODO:
+        HashMap<String, String> hm = new HashMap<>();
+        if (arr.length == 0) {
+            return hm;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length == 0) {
+                continue;
+            }
+
+            if(!hm.containsKey(arr[i][0])) {
+                hm.put(arr[i][0], arr[i][1]);
+            }
+        }
+
+        return hm;
+    }
+}
