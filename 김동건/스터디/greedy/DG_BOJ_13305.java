7e38d58d0009390fcfa6cc2abd3dce0a8c3986de
mukkamukka <rlaehdrjs4605@gmail.com>
2023-06-03 02:08:20 +0900

    Algorithm

---
+++ 김동건/스터디/greedy/DG_BOJ_13305.java
@@ -0,0 +1,39 @@
+package 스터디.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DG_BOJ_13305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] distance = new long[N - 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N - 1; i++) {
+            distance[i] = Long.parseLong(st.nextToken());
+        }
+
+        long[] oilPrice = new long[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            oilPrice[i] = Long.parseLong(st2.nextToken());
+        }
+
+        long totalCost = 0;
+        long minOilPrice = oilPrice[0];
+
+        for (int i = 0; i < N - 1; i++) {
+            if (oilPrice[i] < minOilPrice) {
+                minOilPrice = oilPrice[i];
+            }
+            totalCost += minOilPrice * distance[i];
+        }
+
+        System.out.print(totalCost);
+    }
+}
